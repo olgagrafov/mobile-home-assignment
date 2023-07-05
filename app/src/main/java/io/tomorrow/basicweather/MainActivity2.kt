e3be32822c372fc0fa8b11ad9a734834ae0b3682
OlgaGraf <37404701+olgagrafov@users.noreply.github.com>
2023-07-05 17:14:33 +0300

    Update MainActivity2.kt

--- app/src/main/java/io/tomorrow/basicweather/MainActivity2.kt
+++ app/src/main/java/io/tomorrow/basicweather/MainActivity2.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,7 +15,7 @@ import io.tomorrow.basicweather.locations.ViewModelLocation
 import io.tomorrow.basicweather.ui.theme.BasicWeatherTheme
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.unit.dp
-
+import io.tomorrow.basicweather.locations.FavoriteLocation
 
 
 class MainActivity2 : ComponentActivity() {
@@ -49,13 +50,61 @@ fun SetData(viewModel: ViewModelLocation) {
     } else {
         LazyColumn (modifier = Modifier.padding(5.dp)) {
             items(locations) { item ->
-                Card(modifier = Modifier.padding(5.dp).fillMaxWidth()) {
-                    Text(text = item.name)
-                    Text(text = item.temperature)
-                    Text(text = if (item.isNight)  "night" else "day")
+                Card(modifier = Modifier
+                    .padding(5.dp)
+                    .fillMaxWidth()) {
+                    ClickableItem(item, viewModel)
                 }
             }
         }
     }
 }
+
+@Composable
+fun ClickableItem(item: FavoriteLocation, viewModel: ViewModelLocation) {
+    val itemLocation by viewModel.itemLocation.observeAsState()
+    var isExpanded by remember { mutableStateOf(false) }
+
+    LaunchedEffect(isExpanded) {
+        viewModel.fetchItemLocation(item.id)
+    }
+
+    Box(
+        modifier = Modifier
+            .clickable { isExpanded = true }
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        Column() {
+            Text(text = item.name)
+            Text(text = item.temperature)
+            Text(text = item.localTime)
+            Text(text = if (item.isNight) "night" else "day")
+            if (isExpanded) {
+                itemLocation?.let {
+                    Divider()
+                    Card(modifier = Modifier
+                        .padding(20.dp)
+                        .fillMaxWidth()
+                        .clickable { isExpanded = false }) {
+                        Text(
+                            text = "description: ${it.description}",
+                            modifier = Modifier.padding(top = 8.dp)
+                        )
+                        Text(
+                            text = "humidity: ${it.humidity}",
+                            modifier = Modifier.padding(top = 8.dp)
+                        )
+                        Text(
+                            text = "precipitationProbability: ${it.precipitationProbability}",
+                            modifier = Modifier.padding(top = 8.dp)
+                        )
+                    }
+                }
+            }
+        }
+
+    }
+}
+
 
